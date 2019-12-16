7f936aff63721cd77a5aad88cbcff7851c452c7c
Stefan Ivanovski <stefs.ivanovski@tuta.io>
2019-12-16 20:59:17 +0100

    Panning

--- res/shaders/Mandelbrot/fragment.glsl
+++ res/shaders/Mandelbrot/fragment.glsl
@@ -1,6 +1,8 @@
 #version 460 core
 out vec4 color;
 uniform vec2 windowSize;
+uniform vec2 mousePos;
+uniform float scale;
 
 float map(float current, float oldX, float oldY, float newX, float newY)
 {
@@ -11,10 +13,11 @@ void main()
 {
     vec4 borders = vec4(-2.0, 0.5, -1.25, 1.25);
     int iterations = 100;
+
     vec2 fragPos = gl_FragCoord.xy;
 
-    float cx = map(fragPos.x, 0.0, windowSize.x, borders.x, borders.y);
-    float cy = map(fragPos.y, 0.0, windowSize.y, borders.z, borders.w);
+    float cx = map(fragPos.x, mousePos.x, windowSize.x + mousePos.x, borders.x, borders.y);
+    float cy = map(fragPos.y, mousePos.y, windowSize.y + mousePos.y, borders.z, borders.w);
 
     float zx = 0;
     float zy = 0;
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <glad/glad.h>
 #include "Shader/shader.hpp"
 #include "Display/display.hpp"
+#include <GLFW/glfw3.h>
+#include <cstdio>
+#include <cmath>
 
 int main()
 {
@@ -38,19 +41,50 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    int loc = glGetUniformLocation(basicShader.ID, "windowSize");   // Window size uniform
+    int loc1 = glGetUniformLocation(basicShader.ID, "windowSize");   // Window size uniform
+    int loc2 = glGetUniformLocation(basicShader.ID, "mousePos");
+    int loc3 = glGetUniformLocation(basicShader.ID, "scale");
+
+    float xOffset = 0.0f;
+    float yOffset = 0.0f;
+    float lastX = WIDTH / 2.0f;
+    float lastY = HEIGHT / 2.0f;
+
+    float scale = 1.0f;
 
     // Render loop
     while(display.IsOpen())
     {
         glClear(GL_COLOR_BUFFER_BIT);
-
         glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
+
         if(basicShader.CheckChanged(vertexPath, fragmentPath))
             basicShader.UseShader();
+        
+        double xpos, ypos;
+        glfwGetCursorPos(display.window, &xpos, &ypos);
+
+        if(glfwGetMouseButton(display.window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+        {
+            xOffset += xpos - lastX;
+            yOffset += lastY - ypos;
+        }
+
+        lastX = xpos;
+        lastY = ypos;
+
+        if(glfwGetKey(display.window, GLFW_KEY_KP_ADD))
+            scale++;
+        else if(glfwGetKey(display.window, GLFW_KEY_KP_SUBTRACT))
+            scale--;
+        
+        glUniform1f(loc3, sqrt(scale) / 10.0f);
 
         // Pass screen coordinates to shader
-        glUniform2f(loc, (float) WIDTH, (float) HEIGHT);
+        glUniform2f(loc1, (float) WIDTH, (float) HEIGHT);
+
+        // Pass mouse offset to shader
+        glUniform2f(loc2, xOffset, yOffset);
 
         display.Update();
     }
