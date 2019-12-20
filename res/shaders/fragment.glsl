a47d24de63914a8a7b4573a78f42d8e6c54931d8
Stefan Ivanovski <stefs.ivanovski@tuta.io>
2019-12-20 19:06:28 +0100

    Point light

--- res/shaders/fragment.glsl
+++ res/shaders/fragment.glsl
@@ -11,9 +11,9 @@ float map(float current, float oldX, float oldY, float newX, float newY)
     return (current.x - oldX) / (oldY - oldX) * (newY - newX) + newX;
 }
 
-float sphereSDF(vec3 point)
+float sphereSDF(vec3 point, vec4 sphereData)
 {
-    return length(point) - 1;
+    return length(point - sphereData.xyz) - sphereData.w;
 }
 
 float boxSDF(vec3 point, vec3 box)
@@ -22,56 +22,65 @@ float boxSDF(vec3 point, vec3 box)
     return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0) - 0.1;
 }
 
-float shortestDistance(vec3 origin, vec3 direction)
+float SceneDist(vec3 point)
+{
+    vec4 sphere = vec4(1.0, 1.0, -6.0, 1.0);
+    vec4 sphere2 = vec4(-1.0, 1.0, -5.9, 1.0);
+    float sphereDist1 = sphereSDF(point, sphere);
+    float sphereDist2 = sphereSDF(point, sphere2);
+    float planeDist = point.y;
+
+    float minDist = min(min(sphereDist1, sphereDist2), planeDist);
+    return minDist;
+}
+
+float March(vec3 origin, vec3 direction)
 {
     float currentDistance = 0.0;
     for(int i = 0; i < MAXSTEPS; i++)
     {
         vec3 point = origin + direction * currentDistance;
-        float dist = boxSDF(point, vec3(0.5));
-
-        if(dist < MINDISTANCE) 
-            return currentDistance;
+        float dist = SceneDist(point);
 
         currentDistance += dist;
 
-        if(dist >= MAXDISTANCE)
-            return MAXDISTANCE;
+        if(dist < MINDISTANCE || dist > MAXDISTANCE)
+            break;
     }
 
-    return MAXDISTANCE;
+    return currentDistance;
 }
 
-vec3 rayDirection(float fieldOfView, vec2 size, vec2 fragCoord) {
-    vec2 xy = fragCoord - size / 2.0;
-    float z = size.y / tan(radians(fieldOfView) / 2.0);
-    return normalize(vec3(xy.x, xy.y + 500, -z));
-}
-
-vec3 normal(vec3 point)
+// TODO: Visualize this
+vec3 GetNormal(vec3 point)
 {
-    vec3 box = vec3(0.5);
-    vec3 n = vec3(boxSDF(vec3(point.x + MINDISTANCE, point.y, point.z), box) - boxSDF(vec3(point.x - MINDISTANCE, point.y, point.z), box),
-                  boxSDF(vec3(point.x, point.y + MINDISTANCE, point.z), box) - boxSDF(vec3(point.x, point.y - MINDISTANCE, point.z), box),
-                  boxSDF(vec3(point.x, point.y, point.z + MINDISTANCE), box) - boxSDF(vec3(point.x, point.y, point.z - MINDISTANCE), box));
+    float dist = SceneDist(point);
+    float delta = 0.01;   // Used for secant point2
+
+    vec3 n = vec3(dist - SceneDist(point - vec3(delta, 0.0, 0.0)),
+                  dist - SceneDist(point - vec3(0.0, delta, 0.0)),
+                  dist - SceneDist(point - vec3(0.0, 0.0, delta)));
+
     return normalize(n);
 }
 
 void main()
 {
-    vec3 center = vec3(0.0, -2.0, 10.0);
-    vec3 direction = rayDirection(45.0, windowSize, gl_FragCoord.xy);
-    vec3 lightPos = vec3(0.0, -1.0, 0.0);
+    vec2 uv = (gl_FragCoord.xy - 0.5 * windowSize.xy) / windowSize.y;
 
-    float dist = shortestDistance(center, direction);
-    vec3 n = normal(center + direction*dist);
-    float shading = max(dot(normalize(n - lightPos), n), 0.0);
-    
-    if(dist > MAXDISTANCE - MINDISTANCE)
-    {
-        color = vec4(0.0);
-        return;
-    }
+    vec3 center = vec3(0.0, 1.0, 0.0);
+    vec3 direction = normalize(vec3(uv.x, uv.y, -1));
 
-    color = vec4(shading * vec3(1.0), 1.0);
+    float dist = March(center, direction);
+
+    vec3 point = center + dist*direction;
+
+    // Simple point light
+    vec3 lightPos = vec3(0.0, 10.0, -1.0);
+    vec3 lightDir = normalize(lightPos - point);
+    vec3 normalVector = GetNormal(point);
+    float diffuse = max(0.0, dot(lightDir, normalVector));
+
+    vec3 ambient = vec3(0.0);
+    color = vec4(diffuse * vec3(1.0) + ambient, 1.0);
 }
